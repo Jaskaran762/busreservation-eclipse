c29cc2d4da7ac2504d8c1fe9f1153113228c69d1
Jaskaran762 <singhjaskaran762@gmail.com>
2020-10-01 17:28:54 +0530

    some changes

--- bus-reservation/src/main/java/com/lti/controller/ProfileController.java
+++ bus-reservation/src/main/java/com/lti/controller/ProfileController.java
@@ -1,13 +1,17 @@
 package com.lti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lti.entity.Customer;
 import com.lti.service.ProfileService;
 
+@RestController
+@CrossOrigin
 public class ProfileController {
 
 	@Autowired
